1db6a7f0cddba19c5bea1bbd4d55e1735476382f
alesberesik <beresik.ales@gmail.com>
2022-04-16 15:25:59 +0200

    contacts done

--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
+
 import { MdOutlineEmail } from "react-icons/md";
 import { BsWhatsapp } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
@@ -6,6 +9,27 @@ import { BsLinkedin } from "react-icons/bs";
 import "./Contacts.css";
 
 const Contacts = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_twt8obt",
+        "template_rsvny5r",
+        form.current,
+        "hw9Iz8o4qZWgK3IX4"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
   return (
     <section id="contacts">
       <h5>Get In Touch</h5>
@@ -13,25 +37,57 @@ const Contacts = () => {
       <div className="container contact_container">
         <div className="contact_options">
           <article className="contact_option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className="contact_option-icon" />
             <h4>Email</h4>
             <h5>beresik.ales@gmail.com</h5>
             <a href="mailto:beresik.ales@gmail.com">Send an email</a>
           </article>
           <article className="contact_option">
-            <BsWhatsapp />
-            <h4>Whats app</h4>
-            <h5>Aleš Berešík</h5>
-            <a href="https://m.me/aleš-berešík-b5584b169">Send a message</a>
+            <BsWhatsapp className="contact_option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+421 902 424 187</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+421902424187"
+              target="_blank"
+            >
+              Send a message
+            </a>
           </article>
           <article className="contact_option">
-            <BsLinkedin />
+            <BsLinkedin className="contact_option-icon" />
             <h4>LinkedIn</h4>
             <h5>Aleš Berešík</h5>
-            <a href="https://m.me/">Send a message</a>
+            <a
+              href="https://www.linkedin.com/in/aleš-bereš%C3%ADk-b5584b169/"
+              target="_blank"
+            >
+              Send a message
+            </a>
           </article>
         </div>
-        <form action=""></form>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email Address"
+            required
+          />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          />
+          <button typeof="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,8 +3,32 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <a href="#" className="footer_logo">
+        ALES BERESIK
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experiences">Experiences</a>
+        </li>
+        <li>
+          <a href="#testimonial">Testimonial</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contacts">Contacts</a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer
