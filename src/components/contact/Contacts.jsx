8188d9f2bb06bc0a80547a0d37a6a67b8b4f375e
alesberesik22 <beresik.ales@gmail.com>
2022-04-16 11:51:02 +0200

    projects dones

--- src/components/contact/Contacts.jsx
+++ src/components/contact/Contacts.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import "./Contacts.css"
+import React from "react";
+import { MdOutlineEmail } from "react-icons/md";
+import { BsWhatsapp } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+
+import "./Contacts.css";
 
 const Contacts = () => {
   return (
-    <section id="contacts"></section>
-  )
-}
+    <section id="contacts">
+      <h5>Get In Touch</h5>
+      <h2>Contact me</h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>beresik.ales@gmail.com</h5>
+            <a href="mailto:beresik.ales@gmail.com">Send an email</a>
+          </article>
+          <article className="contact_option">
+            <BsWhatsapp />
+            <h4>Whats app</h4>
+            <h5>Aleš Berešík</h5>
+            <a href="https://m.me/aleš-berešík-b5584b169">Send a message</a>
+          </article>
+          <article className="contact_option">
+            <BsLinkedin />
+            <h4>LinkedIn</h4>
+            <h5>Aleš Berešík</h5>
+            <a href="https://m.me/">Send a message</a>
+          </article>
+        </div>
+        <form action=""></form>
+      </div>
+    </section>
+  );
+};
 
-export default Contacts
+export default Contacts;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
+import React from "react";
+import SmartHomeLogo from "../../assets/images/aleslogo.jpg";
+
+import "./Projects.css";
+
+const data = [
+  {
+    id: 1,
+    image: SmartHomeLogo,
+    title: "Smart Home System",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+  {
+    id: 2,
+    image: SmartHomeLogo,
+    title: "My Portfolio",
+    github: "https://github.com",
+    demo: "https://github.com",
+  },
+];
 
 const Projects = () => {
   return (
-    <section id='projects'>Projects</section>
+    <section id="projects">
+      <h5>Projects</h5>
+      <h2>I have made</h2>
+      <div className="container project_container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return(
+          <article key={id} className="project_item">
+            <div className="project_item-image">
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="project_item-cta">
+              <a href={github} className="btn" target="_blank">
+                Github
+              </a>
+              <a href={demo} className="btn btn-primary" target="_blank">
+                Live Demo
+              </a>
+            </div>
+          </article>
+        );
+        })
+        }
+      </div>
+    </section>
+  );
+};
 
-  )
-}
-
-export default Projects
+export default Projects;
